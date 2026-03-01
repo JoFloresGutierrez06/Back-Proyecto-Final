0f6cca9818867250c2a9632ef3eabe72144aeb9e
Jocelyn Flores <jofloresgutierrez@gmail.com>
2026-02-28 23:08:03 -0600

    cambios

--- src/routes/obras.route.js
+++ src/routes/obras.route.js
@@ -6,12 +6,12 @@ const { authMiddleware, requireRole } = require('../auth')
 const router = express.Router()
 
 router.get("/", asyncHandler(controller.getAllPublish))     // Mostrar obras publicadas 
-router.get("/all", asyncHandler(controller.getAll))         // todo esto se almacena en la variable router
+router.get("/all", authMiddleware, requireRole('admin', 'autor'),asyncHandler(controller.getAll))         // todo esto se almacena en la variable router
 router.get("/search", asyncHandler(controller.search))      // /search?titulo=caperucita+roja&autor=laura+quezada  
 router.get("/:id", asyncHandler(controller.getById))        // Buscar obra por id
 router.post("/", authMiddleware, requireRole('admin', 'autor'), asyncHandler(controller.create))
 // router.post("/", asyncHandler(controller.create))
-router.put("/:id", asyncHandler(controller.update))
-router.delete("/:id", asyncHandler(controller.remove))  
+router.put("/:id", authMiddleware, requireRole('admin', 'autor'), asyncHandler(controller.update))
+router.delete("/:id", authMiddleware, requireRole('admin', 'autor'), asyncHandler(controller.remove))  
 
 module.exports = {router}; // se manda a llamar en el index
